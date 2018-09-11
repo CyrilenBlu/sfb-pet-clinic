206bd6c2f690e1f3a80903a2a52eb44abb4c85c8
CyrilenBlu <lukepetzerint2@gmail.com>
2018-09-11 11:04:51 +0200

    Initial Project Add

---
+++ src/main/java/blue/springframework/sfbpetclinic/SfbPetClinicApplication.java
@@ -0,0 +1,12 @@
+package blue.springframework.sfbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfbPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfbPetClinicApplication.class, args);
+    }
+}
