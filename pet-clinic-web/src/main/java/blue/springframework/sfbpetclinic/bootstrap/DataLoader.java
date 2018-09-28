ac759192e9599b36bbb1b455c2ccbdd18a876ad8
CyrilenBlu <lukepetzerint2@gmail.com>
2018-09-28 12:23:15 +0200

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/blue/springframework/sfbpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/blue/springframework/sfbpetclinic/bootstrap/DataLoader.java
@@ -24,28 +24,24 @@ public class DataLoader implements CommandLineRunner
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Luke");
         owner1.setLastName("Petzer");
 
        ownerService.save(owner1);
 
        Owner owner2 = new Owner();
-       owner2.setId(2L);
        owner2.setFirstName("Azra");
        owner2.setLastName("Parker");
 
        ownerService.save(owner2);
 
        Vet vet1 = new Vet();
-       vet1.setId(1L);
        vet1.setFirstName("Alice");
        vet1.setLastName("Wonderland");
 
        vetService.save(vet1);
 
        Vet vet2 = new Vet();
-       vet2.setId(2L);
        vet2.setFirstName("Bob");
        vet2.setLastName("Ally");
 
