1bd1c4424f0aa79587c6ca23616ed869636e5260
CyrilenBlu <lukepetzerint2@gmail.com>
2018-10-05 11:16:08 +0200

    Added PetType Service and Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package blue.springframework.sfbpetclinic.services;
+
+import blue.springframework.sfbpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>
+{
+
+}
