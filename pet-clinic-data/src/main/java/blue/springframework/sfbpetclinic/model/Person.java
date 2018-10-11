62e7949ee4e4113fc7346c8a3a215f542d850440
CyrilenBlu <lukepetzerint2@gmail.com>
2018-10-11 16:00:59 +0200

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package blue.springframework.sfbpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity
 {
-    private String firstName, lastName;
+    @Column(name = "first_name")
+    private String  firstName;
+    @Column(name = "last_name")
+    private String lastName;
 
     public String getFirstName() {
         return firstName;
