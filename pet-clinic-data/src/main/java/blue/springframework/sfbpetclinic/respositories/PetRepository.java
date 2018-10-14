81b7c93d9c810a2b2d202d91f818bd3be44e278d
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 11:09:22 +0200

    Added Spring Data JPA Service for Pet. Closes #43

--- pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/respositories/PetRepository.java
+++ pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/respositories/PetRepository.java
@@ -1,11 +1,11 @@
 package blue.springframework.sfbpetclinic.respositories;
 
 import blue.springframework.sfbpetclinic.model.Pet;
-import blue.springframework.sfbpetclinic.services.CrudService;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PetRepository extends CrudService<Pet, Long>
+public interface PetRepository extends CrudRepository<Pet, Long>
 {
 
 }
