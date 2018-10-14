de52c90eaeeb528093215b515f10db6301e24c71
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 10:59:33 +0200

    Added Spring Data JPA Service for Vets. Closes #41

--- pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/respositories/SpecialtyRepository.java
+++ pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/respositories/SpecialtyRepository.java
@@ -2,7 +2,9 @@ package blue.springframework.sfbpetclinic.respositories;
 
 import blue.springframework.sfbpetclinic.model.Specialty;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpecialtyRepository extends CrudRepository<Specialty, Long>
 {
 
