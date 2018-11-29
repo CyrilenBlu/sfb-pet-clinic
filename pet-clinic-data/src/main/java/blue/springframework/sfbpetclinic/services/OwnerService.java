1c7ef1646da2126fb2c419e8f0503a5437c6981e
CyrilenBlu <lukepetzerint2@gmail.com>
2018-11-29 14:55:42 +0200

    latest commit

--- pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package blue.springframework.sfbpetclinic.services;
 
 import blue.springframework.sfbpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastName(String lastName);
 }
--- pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/blue/springframework/sfbpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import blue.springframework.sfbpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 @Service
 @Profile({"default", "map"})
@@ -75,5 +76,12 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .findFirst()
                 .orElse(null);
 
+    }
+
+    @Override
+    public List<Owner> findAllByLastName(String lastName) {
+
+        //todo - impl
+        return null;
     }
 }
